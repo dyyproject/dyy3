08d7ba2dfb482a327e505eb0fb019e71ec8bffbb
dyy <dyy@itheima.com>
2019-08-12 22:06:57 +0800

    test

--- src/main/java/com/itheima/test/dyyTest.java
+++ src/main/java/com/itheima/test/dyyTest.java
@@ -8,4 +8,9 @@ public class dyyTest {
         System.out.println("nihao!");
         System.out.println("nihao!");System.out.println("2222!");System.out.println("333!");System.out.println("444!");
     }
+    public void test1(){
+        int i=0;
+        i++;
+        System.out.println(i);
+    }
 }
