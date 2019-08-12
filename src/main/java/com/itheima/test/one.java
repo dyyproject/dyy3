e02649ecbc47f9dc4c6bd238ce19a6491eb5ef3e
dyy <dyy@itheima.com>
2019-08-12 22:57:07 +0800

    one

---
+++ src/main/java/com/itheima/test/one.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class one {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaa");
+        System.out.println("aaaaaaa");
+        System.out.println("aaaaaaa");
+        System.out.println("aaaaaaa");
+        System.out.println("aaaaaaa");
+    }
+}
