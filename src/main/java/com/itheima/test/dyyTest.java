ed993a0f68b1be264893e343433fb0be54faa98e
dyy <dyy@itheima.com>
2019-08-12 21:58:06 +0800

    本地添加

---
+++ src/main/java/com/itheima/test/dyyTest.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class dyyTest {
+    public static void main(String[] args) {
+        System.out.println("nihao!");
+        System.out.println("nihao!");
+        System.out.println("nihao!");
+        System.out.println("nihao!");
+        System.out.println("nihao!");
+    }
+}
